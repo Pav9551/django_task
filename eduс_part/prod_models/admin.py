39b4cf609f64958c85c7d08eb8ee871ece1de86b
pav9551 <pav9551@yandex.ru>
2024-02-29 13:02:15 +0300

    Added er-diagram

--- eduс_part/prod_models/admin.py
+++ eduс_part/prod_models/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Product, Lesson, Group
+from .models import Product, Lesson, Group, UserProduct
 
 admin.site.register(Product)
 admin.site.register(Lesson)
 admin.site.register(Group)
+admin.site.register(UserProduct)
--- eduс_part/prod_models/models.py
+++ eduс_part/prod_models/models.py
@@ -9,14 +9,32 @@ class Product(models.Model):
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     min_users_per_group = models.PositiveIntegerField()
     max_users_per_group = models.PositiveIntegerField()
+    def __str__(self):
+        return f' {self.name}'
 
 class Lesson(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     video_URL = models.URLField()
+    def __str__(self):
+        return f'{self.name} - {self.product.name}'
+
+class UserProduct(models.Model):
+    # Пользователь
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_products')
+    # Продукт
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='user_products')
+    # Дата и время покупки
+    purchase_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f'{self.user.username} - {self.product.name}'
 
 class Group(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(UserProduct, on_delete=models.CASCADE)
     users = models.ManyToManyField(User, related_name='educ_groups')
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.name}'
+
 
